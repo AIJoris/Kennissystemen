a87d3a4f433c05e0b263398f534dc04aa4c8347d
Haitam <haitam_benyahia@hotmail.com>
2015-04-28 12:03:30 +0200

    push

--- KS4/KS4.pl
+++ KS4/KS4.pl
@@ -9,21 +9,39 @@
 :- op(800, xfy, concurrent).
 :- op(200, xfy, or).
 
+
+%% The timeline in this case is a-b-c
 go1:-
 	assert(event(a)),
 	assert(event(b)),
 	assert(event(c)),
-	a before b,
-	c before a.
+	assert(a before b),
+	assert(b before c).
 
-% go2 aanpassen
+% The possible timelines are the following
+% c-b-a
+% b-c-a
 go2:-
 	assert(event(a)),
 	assert(event(b)),
 	assert(event(c)),
-	a before b,
+	a after b,
 	c before a.
 
 foo(A or B):-
 	foo(A);
 	foo(B).
+% Transitive before rule
+transitive_before(X, Y, Operator) :-
+	 X before Y.
+
+transitive_before(X, Z, Operator) :- 
+	X before Y,
+	transitive_before(Y, Z, Operator),
+	\+ X before Z,
+	assert(X before Z).
+
+
+
+
+
